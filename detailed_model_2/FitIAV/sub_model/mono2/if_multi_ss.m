abd30e2f85c0ed1bc7128e4ffd54871499e99691
jxzhou <zjx1996@pku.edu.cn>
2022-09-14 16:12:05 +0800

    migrate as hill function for mono and CCL2 model

---
+++ detailed_model_2/FitIAV/sub_model/mono2/if_multi_ss.m
@@ -0,0 +1,21 @@
+function multi_ss = if_multi_ss(par_base, par_consider_idx, par_consider)
+    
+    multi_ss = false;
+    
+    % load initial values
+    y0s = importdata('lhs_init.txt');
+    [m, n] = size(y0s);
+    
+    ys = zeros(m, n);
+    
+    parfor i = 1:m
+        ys(i, :) = solve_odes(par_base, par_consider_idx, par_consider, y0s(i, :));
+    end
+    
+    for i = 1:n
+        if ((max(ys(:, i)) - min(ys(:, i))) / max(ys(:, i)) > 1e-2)
+            multi_ss = true;
+        end
+    end
+    
+end
