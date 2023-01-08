c96d21dd95b266b06c8fca8803469c2fbeb88b0d
jxzhou <zjx1996@pku.edu.cn>
2023-01-08 20:56:41 +0800

    bifurcation diagram

--- detailed_model_v6/bifurcation/plotxppaut.m
+++ detailed_model_v6/bifurcation/plotxppaut.m
@@ -181,100 +181,103 @@ end
 % 
 % 
 % 
-% [file_in,path] = uigetfile('*.dat','.dat file saved by AUTO (XPPAUT) ');
-% file_name = [path file_in];
-% 
-% if(file_name)
-%     st = importdata(file_name);
-% 
-% %     [file_in2,path2] = uigetfile('*.dat','.dat file saved by AUTO (XPPAUT) ');
-% %     file_name2 = [path2 file_in2];
-% %     st2 = importdata(file_name2);
-%   
-% %   temp=[st; st2];
-%     temp = st;
-% 
-% %     for i = 1:length(temp)
-% %         if temp(i, 2) < 1e-3
-% %             temp(i, 2) = 1e-3;
-% %         end
-% %         if temp(i, 3) <1e-3
-% %             temp(i, 3) = 1e-3;
-% %         end
-% %     end
-% 
-% %   threshold_bool = false(size(temp, 1), 1);
-% %   for i=1:size(temp, 1)
-% %       if (temp(i, 1) > 0.2) || (temp(i, 1) < 0)
-% %            threshold_bool(i) = true;
-% %       end
-% %   end
-% %   temp(threshold_bool, :) = [];
-%   
-%   temp1=zeros(size(temp,1),8)*NaN;
-%   
-%   for n=1:size(temp1,1)
-%     if(temp(n,4)==1)
-%       temp1(n,[1 5]) = temp(n,[2 3]);
+[file_in,path] = uigetfile('*.dat','.dat file saved by AUTO (XPPAUT) ');
+file_name = [path file_in];
+
+if(file_name)
+    st = importdata(file_name);
+
+%     [file_in2,path2] = uigetfile('*.dat','.dat file saved by AUTO (XPPAUT) ');
+%     file_name2 = [path2 file_in2];
+%     st2 = importdata(file_name2);
+  
+%   temp=[st; st2];
+    temp = st;
+
+%     for i = 1:length(temp)
+%         if temp(i, 2) < 1e-3
+%             temp(i, 2) = 1e-3;
+%         end
+%         if temp(i, 3) <1e-3
+%             temp(i, 3) = 1e-3;
+%         end
 %     end
-%     if(temp(n,4)==2)
-%       temp1(n,[2 6]) = temp(n,[2 3]);
-%     end
-%     if(temp(n,4)==3)
-%       temp1(n,[3 7]) = temp(n,[2 3]);
-%     end
-%     if(temp(n,4)==4)
-%       temp1(n,[4 8]) = temp(n,[2 3]);
-%     end
+
+%   threshold_bool = false(size(temp, 1), 1);
+%   for i=1:size(temp, 1)
+%       if (temp(i, 1) > 0.2) || (temp(i, 1) < 0)
+%            threshold_bool(i) = true;
+%       end
 %   end
-%   
-%   
-% % $$$ figure;
-% %	-----------------------------------------  
-%   h_SS = plot(temp(:,1),temp1(:,[1 5]));
-%   set(h_SS,'color',C_Ss)
-%   set(h_SS,'linestyle',Lt_Ss)
-%   set(h_SS,'linewidth',Lw_Ss)
-%   set(h_SS,'Markersize',Ms_Ss)
-%   set(h_SS,'MarkerFacecolor',Mfc_Ss)
-%   hold on;
-% 
-%   
-% %	-----------------------------------------  
-%   h_US = plot(temp(:,1),temp1(:,[2 6]));
-%   set(h_US,'color',C_Us)
-%   set(h_US,'linestyle',Lt_Us)
-%   set(h_US,'linewidth',Lw_Us)
-%   set(h_US,'Markersize',Ms_Us)
-%   set(h_US,'MarkerFacecolor',Mfc_Us)
-% 
-% %	-----------------------------------------  
-%   
-% 
-% h_SP = plot(temp(:,1),temp1(:,[3 7]));
-%   set(h_SP,'color',C_Sp)
-%   set(h_SP,'linestyle',Lt_Sp)
-%   set(h_SP,'linewidth',Lw_Sp)
-%   set(h_SP,'Markersize',Ms_Sp)
-%   set(h_SP,'MarkerFacecolor',Mfc_Sp)
-% %	-----------------------------------------   
-%   
-%   
-%   h_UP = plot(temp(:,1),temp1(:,[4 8]));
-%   set(h_UP,'color',C_Up)
-%   set(h_UP,'linestyle',Lt_Up)
-%   set(h_UP,'linewidth',Lw_Up)
-%   set(h_UP,'Markersize',Ms_Up)
-%   set(h_UP,'MarkerFacecolor',Mfc_Up)
-% %	-----------------------------------------    
-%   
-%   
-%   
-%   set(gca, 'FontSize', 26); box on;
-% %   set(gca, 'YScale', 'log');
-%   xlim([0 2]);
-%   xlabel('c_{M-IL10}');
-%   ylabel('CCL2');
-%   hold off;
-% %   title(file_in);
-% end
+%   temp(threshold_bool, :) = [];
+  
+  temp1=zeros(size(temp,1),8)*NaN;
+  
+  for n=1:size(temp1,1)
+    if(temp(n,4)==1)
+      temp1(n,[1 5]) = temp(n,[2 3]);
+    end
+    if(temp(n,4)==2)
+      temp1(n,[2 6]) = temp(n,[2 3]);
+    end
+    if(temp(n,4)==3)
+      temp1(n,[3 7]) = temp(n,[2 3]);
+    end
+    if(temp(n,4)==4)
+      temp1(n,[4 8]) = temp(n,[2 3]);
+    end
+  end
+  
+  
+% $$$ figure;
+%	-----------------------------------------  
+  h_SS = plot(temp(:,1),temp1(:,[1 5]));
+  set(h_SS,'color',C_Ss)
+  set(h_SS,'linestyle',Lt_Ss)
+  set(h_SS,'linewidth',Lw_Ss)
+  set(h_SS,'Markersize',Ms_Ss)
+  set(h_SS,'MarkerFacecolor',Mfc_Ss)
+  hold on;
+
+  
+%	-----------------------------------------  
+  h_US = plot(temp(:,1),temp1(:,[2 6]));
+  set(h_US,'color',C_Us)
+  set(h_US,'linestyle',Lt_Us)
+  set(h_US,'linewidth',Lw_Us)
+  set(h_US,'Markersize',Ms_Us)
+  set(h_US,'MarkerFacecolor',Mfc_Us)
+
+%	-----------------------------------------  
+  
+
+h_SP = plot(temp(:,1),temp1(:,[3 7]));
+  set(h_SP,'color',C_Sp)
+  set(h_SP,'linestyle',Lt_Sp)
+  set(h_SP,'linewidth',Lw_Sp)
+  set(h_SP,'Markersize',Ms_Sp)
+  set(h_SP,'MarkerFacecolor',Mfc_Sp)
+%	-----------------------------------------   
+  
+  
+  h_UP = plot(temp(:,1),temp1(:,[4 8]));
+  set(h_UP,'color',C_Up)
+  set(h_UP,'linestyle',Lt_Up)
+  set(h_UP,'linewidth',Lw_Up)
+  set(h_UP,'Markersize',Ms_Up)
+  set(h_UP,'MarkerFacecolor',Mfc_Up)
+%	-----------------------------------------    
+  
+  
+  
+  set(gca, 'FontSize', 12); box on;
+%   set(gca, 'YScale', 'log');
+  xlim([0 3]);
+  ylim([-10 400]);
+  xlabel('c_{M-CCL2}');
+  ylabel('CCL2');
+  set(gcf, 'unit', 'centimeters', 'position', [10 5 8 6]);
+  plot([1.8 1.8], [-10 400], '--', 'Color', '#DF392A', 'LineWidth', 1.5);
+  hold off;
+%   title(file_in);
+end
